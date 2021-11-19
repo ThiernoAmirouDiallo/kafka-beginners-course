d4b89a3453a5e2b81a559a3981c4b8f1b6023282
Thierno Diallo <thiernoamiroudiallo@lyft.com>
2021-11-18 23:30:18 -0500

    seding bulk requests to elasticsearch

--- src/main/java/com/kafka/thierno/elasticsearch/KafkaConsumerElasticSearch.java
+++ src/main/java/com/kafka/thierno/elasticsearch/KafkaConsumerElasticSearch.java
@@ -12,6 +12,7 @@ import com.google.gson.JsonParser;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.UUID;
@@ -25,6 +26,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -60,19 +63,19 @@ public class KafkaConsumerElasticSearch {
 
 		//properties
 		Properties properties = new Properties();
-		properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-		properties.setProperty(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(GROUP_ID_CONFIG, groupId);
-		properties.setProperty(AUTO_OFFSET_RESET_CONFIG, "earliest");
-		properties.setProperty(ENABLE_AUTO_COMMIT_CONFIG, "false"); //disable auto commit of offsets
-		properties.setProperty(MAX_POLL_RECORDS_CONFIG, "20");
+		properties.setProperty( BOOTSTRAP_SERVERS_CONFIG, bootstrapServers );
+		properties.setProperty( KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName() );
+		properties.setProperty( VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName() );
+		properties.setProperty( GROUP_ID_CONFIG, groupId );
+		properties.setProperty( AUTO_OFFSET_RESET_CONFIG, "earliest" );
+		properties.setProperty( ENABLE_AUTO_COMMIT_CONFIG, "false" ); //disable auto commit of offsets
+		properties.setProperty( MAX_POLL_RECORDS_CONFIG, "20" );
 
 		//consumer
-		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>( properties );
 
 		//subscribe consumer to topics
-		consumer.subscribe( Collections.singletonList(topic));
+		consumer.subscribe( Collections.singletonList( topic ) );
 
 		return consumer;
 	}
@@ -86,11 +89,15 @@ public class KafkaConsumerElasticSearch {
 
 		//poll new data
 		while (true) {
-			ConsumerRecords<String, String> records = consumer.poll( Duration.ofMillis(200));
+			ConsumerRecords<String, String> records = consumer.poll( Duration.ofMillis( 200 ) );
 
 			logger.info( "Received {} kafka records", records.count() );
-			for ( ConsumerRecord<String, String> record : records) {
-				logger.info("Pushing to elasticsearch:\n\tKey --> {} \n\tValue: {}\ttopic: {}\toffset: {}\n\tpartition: {}\n\ttimestamp: {}", record.key(), record.value(), record.topic(), record.offset(), record.partition(), record.timestamp());
+
+			BulkRequest bulkRequest = new BulkRequest();
+
+			for ( ConsumerRecord<String, String> record : records ) {
+				logger.info( "Pushing to elasticsearch:\n\tKey --> {} \n\tValue: {}\ttopic: {}\toffset: {}\n\tpartition: {}\n\ttimestamp: {}", record.key(), record.value(), record.topic(),
+						record.offset(), record.partition(), record.timestamp() );
 
 				/*
 				 2 strategies for id,
@@ -100,16 +107,23 @@ public class KafkaConsumerElasticSearch {
 				 */
 				String id = extractTweetId( record.value() );
 
-				pushElasticSearch( id, record.value(), XContentType.JSON );
+				//pushElasticSearch( id, record.value(), XContentType.JSON ); // to push one record at a time to elastic search (not very performant, see bulkRequest below)
+				bulkRequest.add( //
+						new IndexRequest( "twitter" ) //
+								.type( "tweets" ) //
+								.id( id ) // to make to kafka consumer idenpotent because by default the the develivery-semantic is at-least-one meaning the same kafka record can be read twice
+								.source( record.value(), XContentType.JSON ) );
+			}
+
+			if ( records.count() > 0 ) {
+				bulkPushElasticSearch( bulkRequest ); //send multiple request to elasticsearch at once
+
+				logger.info( "Commiting offsets" );
+				consumer.commitSync();
+				logger.info( "Offsets have been committed" );
 
 				Thread.sleep( 1000 ); // adding a small delay
 			}
-
-			logger.info( "Commiting offsets" );
-			consumer.commitSync();
-			logger.info( "Offsets have been committed" );
-
-			Thread.sleep( 1000 ); // adding a small delay
 		}
 	}
 
@@ -120,14 +134,23 @@ public class KafkaConsumerElasticSearch {
 	private static void pushElasticSearch(String id, String source, XContentType contentType) {
 		IndexRequest indexRequest = new IndexRequest( "twitter" ) //
 				.type( "tweets" ) //
-				.id(id) // to make to kafka consumer idenpotent because by default the the develivery-semantic is at-least-one meaning the same kafka record can be read twice
-				.source( source, contentType);
+				.id( id ) // to make to kafka consumer idenpotent because by default the the develivery-semantic is at-least-one meaning the same kafka record can be read twice
+				.source( source, contentType );
 
 		try ( RestHighLevelClient client = restHighLevelClient() ) {
 			IndexResponse indexResponse = client.index( indexRequest, RequestOptions.DEFAULT );
 			logger.info( "Response id: {}, response status: {}\n\n", indexResponse.getId(), indexResponse.status().name() );
 		} catch ( IOException e ) {
 			logger.error( "Error while creating elasticsearch index", e );
+		}
+	}
+
+	private static void bulkPushElasticSearch( BulkRequest bulkRequest ) {
+		try ( RestHighLevelClient client = restHighLevelClient() ) {
+			BulkResponse bulkResponse = client.bulk( bulkRequest, RequestOptions.DEFAULT );
+			logger.info( "BulkResponse itemCount: {}, response status: {}\n\n", Arrays.stream( bulkResponse.getItems() ).count(), bulkResponse.status().name() );
+		} catch ( IOException e ) {
+			logger.error( "Error while sending bulkrequest to elasticsearch", e );
 		}
 	}
 
